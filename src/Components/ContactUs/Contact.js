352203d7afcd181ea65fdae33e2a04c7fdab69c0
shivam gurunani <namisha.gurunani173261@gmail.com>
2024-01-18 16:15:35 +0530

    done

--- src/Components/ContactUs/Contact.js
+++ src/Components/ContactUs/Contact.js
@@ -7,9 +7,9 @@ const Contact = () => {
       <section className="contact">
       <div className="container">
         <h1>Contact Me</h1>
-        <p>Created by : Mayank Gupta</p>
-        <p>Email Address : mayankkatheriya4@gmail.com</p>
-        <p>Linked in link : <a href="https://www.linkedin.com/in/mayank-gupta-752328173/" target='_blank'> https://www.linkedin.com/in/mayank-gupta-752328173/ </a></p>
+        <p>Created by : Namisha Gurunani</p>
+        <p>Email Address : namisha.gurunani173261@gmail.com</p>
+        <p>Linked in link : <a href="https://www.linkedin.com/in/namisha-gurunani-82808025b" target='_blank'> https://www.linkedin.com/in/mayank-gupta-752328173/ </a></p>
       </div>
     </section>
     </main>
--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
   console.log("Footer");
   return (
     <footer>
-        Copyright@2024 | All Rights Reserved | by <a href="https://github.com/Mayankkatheriya" target="_blank" rel="noopener noreferrer">Mayank Gupta</a>
+        Copyright@2024 | All Rights Reserved | by <a href="https://github.com/namishagurunani" target="_blank" rel="noopener noreferrer">Namisha Gurunani</a>
     </footer>
   )
 }
